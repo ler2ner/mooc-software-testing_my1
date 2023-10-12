3840292c873027edb51a554454cca14ce5fbcb6d
ler2ner <ler2ner@yandex.com>
2023-10-12 19:40:11 +0300

    Complete 2 the test class for addFive

---
+++ src/main/java/tudelft/gettingstarted/Main.java
@@ -0,0 +1,17 @@
+package tudelft.gettingstarted;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("Num + 5 ="+GettingStarted.addFive(22));
+		
+		GettingStarted gs = new GettingStarted();
+		
+		System.out.println(gs.addFive(-20));
+		
+
+	}
+
+}
